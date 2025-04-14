29e5e45225b6a8b7df60d77945ed6f10a6f11c85
Shirish Koirala <shirishkoirala@gmail.com>
2025-04-15 08:25:48 +1000

    Orientation fixed.

--- app/src/main/java/app/mjproductions/turboscratch/MainActivity.kt
+++ app/src/main/java/app/mjproductions/turboscratch/MainActivity.kt
@@ -1,10 +1,10 @@
 package app.mjproductions.turboscratch
-import android.widget.Toast
 
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
